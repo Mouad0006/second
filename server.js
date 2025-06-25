44cb32fbd4cd6c2ec0519fd02e23953b74060789
Mouad0006 <jamalfahal6@gmail.com>
2025-06-25 15:01:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -377,7 +377,7 @@ app.get('/', requireLogin, (req, res) => {
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <title>SAMURAI LOG - MILANO 2200</title>
+  <title>SAMURAI LOG - MILANO</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://fonts.googleapis.com/css?family=Montserrat:700|Noto+Sans+JP:wght@900&display=swap" rel="stylesheet">
   <style>
